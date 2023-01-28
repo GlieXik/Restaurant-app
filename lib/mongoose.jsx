896d7e900502be6d2872d65b8078bc96063aa9ba
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-28 21:43:10 +0200

    strictQuery

--- lib/mongoose.jsx
+++ lib/mongoose.jsx
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
-const MONGOSSE_URI = process.env.MONGOSSE_URI;
-
 export default async function dbConnect() {
+  const MONGOSSE_URI = process.env.MONGOSSE_URI;
+
+  mongoose.set("strictQuery", false);
+
   mongoose.connect(MONGOSSE_URI);
 }
