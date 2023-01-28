038dc6249b052f07458be4c99dda9d4a62cc6b62
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-28 16:50:02 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    secondSecret: process.env.MONGO_URI,
+  },
 };
 
 module.exports = nextConfig;
