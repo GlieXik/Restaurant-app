c26f9466f0549105ab67be9bdfa496286261540b
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-29 22:43:33 +0200

    extra minimal

--- pages/api/like.js
+++ pages/api/like.js
@@ -1,23 +1,23 @@
-async function addLike(id) {
-  return Menu.findOneAndUpdate({ _id: id }, { $inc: { like: 1 } });
-}
-async function delLike(id) {
-  return Menu.findOneAndUpdate({ _id: id }, { $inc: { like: -1 } });
-}
+// async function addLike(id) {
+//   return Menu.findOneAndUpdate({ _id: id }, { $inc: { like: 1 } });
+// }
+// async function delLike(id) {
+//   return Menu.findOneAndUpdate({ _id: id }, { $inc: { like: -1 } });
+// }
 
-export default async function likeOnMongo(req, res) {
-  try {
-    const { id } = req.query;
-    if (req.method === "PUT") {
-      const add = await addLike(id);
-      res.status(200).json({ ok: "k" });
-    } else if (req.method === "DELETE") {
-      const del = await delLike(id);
-      res.status(200).send(del);
-    } else {
-      res.status(405).json({ message: "Bad method" });
-    }
-  } catch (e) {
-    res.status(500).send();
-  }
-}
+// export default async function likeOnMongo(req, res) {
+//   try {
+//     const { id } = req.query;
+//     if (req.method === "PUT") {
+//       const add = await addLike(id);
+//       res.status(200).json({ ok: "k" });
+//     } else if (req.method === "DELETE") {
+//       const del = await delLike(id);
+//       res.status(200).send(del);
+//     } else {
+//       res.status(405).json({ message: "Bad method" });
+//     }
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// }
