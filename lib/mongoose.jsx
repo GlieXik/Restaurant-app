71d44ca5825c8a93ee41dc6a60e8b6b95582bc97
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-29 17:06:11 +0200

    mongoosee

--- lib/mongoose.jsx
+++ lib/mongoose.jsx
@@ -8,11 +8,41 @@ if (!MONGODB_URI) {
   );
 }
 
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function dbConnect() {
-  mongoose.connect(MONGODB_URI).then((mongoose) => {
-    console.log("con");
-    return mongoose;
-  });
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("Just con");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
 
 export default dbConnect;
