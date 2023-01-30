71c8833832f7cf5d154b798afafe18df6138626a
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-30 11:20:45 +0200

    add mongoClient ssg

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head";
-
+import clientPromise from "@/lib/mongodb";
 import { MenuCom } from "@/components/Menu/Menu";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material";
 // import ListMenu from "@/components/Menu/ListMenu/ListMenu";
 import InfoPanel from "@/components/InfoPanel/InfoPanel";
-
-// import clientPromise from "@/lib/mongodb";
 
 // import axios from "axios";
 // import { fetching } from "@/utils/fetch";
@@ -48,40 +46,16 @@ export default function Home({ menu }) {
 
 export async function getServerSideProps(ctx) {
   try {
-    // const mongoClient = await clientPromise;
+    const mongoClient = await clientPromise;
     // const res = await fetching("api/menu");
 
-    // const db = mongoClient.db("duplomna");
-    // const collection = db.collection("menus");
-    // const results = await collection.find({}).toArray();
+    const db = mongoClient.db("duplomna");
+    const collection = db.collection("menus");
+    const results = await collection.find({}).toArray();
 
-    // console.log(res);
     return {
       props: {
-        menu: [
-          {
-            name: "feds",
-            price: 85,
-            image: "/menuImg/free.jpeg",
-            description: "dsadafdsadsfadfsfdsafdss",
-            weigth: 300,
-            persent_alcho: 4.2,
-            category: "Холодна закуска",
-            type: "Кухня",
-            like: 0,
-          },
-          {
-            name: "feds",
-            price: 85,
-            image: "/menuImg/free.jpeg",
-            description: "dsdsafdfgsssáadas",
-            weigth: 300,
-            persent_alcho: 4.2,
-            category: "Холодна закуска",
-            type: "Кухня",
-            like: 0,
-          },
-        ],
+        menu: JSON.parse(JSON.stringify(results)),
       },
     };
   } catch (error) {
