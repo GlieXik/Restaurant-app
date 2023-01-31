f2b496d057174650d93b3fb0b590047274a77ed3
Stefan Dunikovskyi <stefan090304@gmail.com>
2023-01-31 22:02:48 +0200

    fix icon + base64

--- components/Menu/ListMenu/ListMenu.jsx
+++ components/Menu/ListMenu/ListMenu.jsx
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 
 import ScaleIcon from "@mui/icons-material/Scale";
 import LocalBarIcon from "@mui/icons-material/LocalBar";
+import LocalDrinkIcon from "@mui/icons-material/LocalDrink";
 import Image from "next/image";
 import { nanoid } from "nanoid";
 import { Fragment } from "react";
@@ -15,6 +16,25 @@ import Like from "./Like";
 import filteByMenu from "@/utils/filterByMenuCategotyes";
 
 const ListMenu = ({ menu }) => {
+  const convertImage = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+  const toBase64 = (str) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+
   const renderMenu = (datas) => {
     return datas.map(({ category, data }) => (
       <Fragment key={nanoid()}>
@@ -36,7 +56,17 @@ const ListMenu = ({ menu }) => {
         </Element>
 
         {data.map(
-          ({ name, description, weigth, image, persent_alcho, like, _id }) => {
+          ({
+            name,
+            description,
+            weigth,
+            image,
+            persent_alcho,
+            like,
+            _id,
+            type,
+            price,
+          }) => {
             return (
               <Card
                 key={nanoid()}
@@ -53,12 +83,21 @@ const ListMenu = ({ menu }) => {
                   }}
                 >
                   <Box>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography variant="h5" component="div">
                       {name}
                     </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      sx={{ color: "rgb(255, 161, 27)" }}
+                      gutterBottom
+                    >
+                      {price} грн
+                    </Typography>
+
                     <Typography variant="body2" sx={{ mb: 1.5 }}>
                       {description}
                     </Typography>
+
                     <Box
                       sx={{
                         display: "flex",
@@ -67,7 +106,7 @@ const ListMenu = ({ menu }) => {
                         marginBottom: 1,
                       }}
                     >
-                      {weigth && (
+                      {weigth > 0 && type === "Кухня" && (
                         <>
                           <ScaleIcon sx={{ fontSize: 12 }} />
                           <Typography sx={{ fontSize: 12 }}>
@@ -75,7 +114,15 @@ const ListMenu = ({ menu }) => {
                           </Typography>
                         </>
                       )}
-                      {persent_alcho > 0 && (
+                      {weigth > 0 && type === "Бар" && (
+                        <>
+                          <LocalDrinkIcon sx={{ fontSize: 12 }} />
+                          <Typography sx={{ fontSize: 12 }}>
+                            {weigth}л
+                          </Typography>
+                        </>
+                      )}
+                      {persent_alcho > 0 && type === "Бар" && (
                         <>
                           <LocalBarIcon sx={{ fontSize: 12 }} />
                           <Typography sx={{ fontSize: 12 }}>
@@ -90,10 +137,14 @@ const ListMenu = ({ menu }) => {
                   <Box>
                     <Image
                       src={image}
-                      alt="Picture of the author"
+                      alt="Picture"
                       width={120}
                       height={90}
                       style={{ borderRadius: "0.2rem" }}
+                      placeholder="blur"
+                      blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                        convertImage(120, 90)
+                      )}`}
                     />
                   </Box>
                 </CardContent>
@@ -104,7 +155,6 @@ const ListMenu = ({ menu }) => {
       </Fragment>
     ));
   };
-
   return <>{renderMenu(filteByMenu(menu))}</>;
 };
 
